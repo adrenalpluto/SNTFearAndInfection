86b5f0c4e871b3ca03a6371e4a92daea4d6df69d
Adrenalpluto <adrenalpluto@gmail.com>
2026-05-14 17:50:00 -0400

    changed water wheel recipe

--- kubejs/server_scripts/Create/recipes.js
+++ kubejs/server_scripts/Create/recipes.js
@@ -42,6 +42,28 @@ event.shapeless(
   ]
 )
 
+event.shaped('create:water_wheel', [// arg 1: output
+  'RTR',
+  'TST',
+  'RTR'  
+], {
+  T: 'kubejs:treated_planks',
+  R: 'clanginghowl:steel_rod',
+  S: 'create:shaft'
+}
+)
+
+event.shaped('create:large_water_wheel', [// arg 1: output
+  'RTR',
+  'TWT',
+  'RTR'  
+], {
+  T: 'kubejs:treated_planks',
+  R: 'clanginghowl:steel_rod',
+  W: 'create:water_wheel'
+}
+)
+
 event.remove({ id: 'create:item_application/andesite_casing_from_wood' })
 event.remove({ id: 'create:crafting/materials/andesite_alloy' })
 event.remove({ id: 'create:crafting/materials/andesite_alloy_from_zinc' })
@@ -51,6 +73,8 @@ event.remove({ id: 'create:crafting/kinetics/shaft' })
 event.remove({ id: 'create:crafting/kinetics/cogwheel' })
 event.remove({ id: 'create:crafting/kinetics/large_cogwheel' })
 event.remove({ id: 'create:crafting/kinetics/large_cogwheel_from_little' })
+event.remove({ id: 'create:crafting/kinetics/large_water_wheel' })
+event.remove({ id: 'create:crafting/kinetics/water_wheel' })
 
 })
 
