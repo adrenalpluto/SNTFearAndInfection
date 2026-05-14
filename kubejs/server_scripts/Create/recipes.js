b8198c47032b83f2bde53800b4899c06e5f75d55
Adrenalpluto <adrenalpluto@gmail.com>
2026-05-14 17:28:54 -0400

    changes cogs recipe

--- kubejs/server_scripts/Create/recipes.js
+++ kubejs/server_scripts/Create/recipes.js
@@ -17,12 +17,40 @@ event.shaped('kubejs:treated_planks', [// arg 1: output
 }
 )
 
+event.shapeless(
+  Item.of('create:cogwheel'), // arg 1: output
+  [
+    'create:shaft',
+    'kubejs:treated_planks'
+  ]
+)
+
+event.shapeless(
+  Item.of('create:large_cogwheel'), // arg 1: output
+  [
+    'create:shaft',
+    'kubejs:treated_planks',
+    'kubejs:treated_planks'
+  ]
+)
+
+event.shapeless(
+  Item.of('create:large_cogwheel'), // arg 1: output
+  [
+    'create:cogwheel',
+    'kubejs:treated_planks'
+  ]
+)
+
 event.remove({ id: 'create:item_application/andesite_casing_from_wood' })
 event.remove({ id: 'create:crafting/materials/andesite_alloy' })
 event.remove({ id: 'create:crafting/materials/andesite_alloy_from_zinc' })
 event.remove({ id: 'create:mixing/andesite_alloy_from_zinc' })
 event.remove({ id: 'create:mixing/andesite_alloy' })
 event.remove({ id: 'create:crafting/kinetics/shaft' })
+event.remove({ id: 'create:crafting/kinetics/cogwheel' })
+event.remove({ id: 'create:crafting/kinetics/large_cogwheel' })
+event.remove({ id: 'create:crafting/kinetics/large_cogwheel_from_little' })
 
 })
 
